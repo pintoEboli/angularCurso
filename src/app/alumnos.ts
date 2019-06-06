5ec2bd47d0317e700182bb8e4b882b8498214f95
David Morales <dmoralesnet@hotmail.com>
2019-06-06 23:10:45 +0200

    Create alumnos.ts
    Clase Alumno

---
+++ src/app/alumnos.ts
@@ -0,0 +1,18 @@
+export class Alumnos {
+
+    nombre: string;
+    apellido: string;
+    edad: number;
+    curso: number;
+    fecha: Date;
+    telefono: number;
+
+    
+    constructor(nombre:string,apellido:string,edad:number,curso:number,fecha:Date,telefono:number){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.curso = curso;
+        this.fecha = fecha;
+        this.telefono = telefono;
+    }
